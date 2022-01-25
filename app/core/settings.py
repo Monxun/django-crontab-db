ec102607e0732423fd85ff7b399ff0328f6643ab
Monxun <michaelgibney1@gmail.com>
2022-01-25 01:26:46 -0700

    update

--- app/core/settings.py
+++ app/core/settings.py
@@ -10,13 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import environ
+import os
 from pathlib import Path
 
-env = environ.Env()
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -41,6 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'django_crontab',
+
+    # CUSTOM APPS
+    'cron',
 
 ]
 
